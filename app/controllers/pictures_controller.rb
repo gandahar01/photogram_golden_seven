78640c8c01cf9b5408d9a9dd9024e1994c9c0596
Shem Boduch <pbboduch@chicagobooth.edu>
2017-07-20 00:36:38 +0000

    photo inprogress v2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,65 @@ class PicturesController < ApplicationController
   def new_form
     render("pic_templates/new_form.html.erb")
   end
+
+  def create_row
+    p = Photo.new
+
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+
+    p.save
+
+    @current_count = Photo.count
+
+    render("pic_templates/create_row.html.erb")
+  end
+
+  def index
+    @list_of_photos = Photo.order(:created_at => :desc)
+
+    render("pic_templates/index.html.erb")
+  end
+
+  def show
+    # Here are the Parameters: {"an_id"=>"5"}
+    
+
+    
+    @pic = Photo.find(params["an_id"])
+    
+    @the_source = pic.source
+    @the_caption = pic.caption
+
+    render("pic_templates/show.html.erb")
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:some_id])
+
+    render("pic_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    p = Photo.find(params[:the_id])
+
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+
+    p.save
+
+    @the_id = p.id
+
+    render("pic_templates/update_row.html.erb")
+  end
+
+  def destroy_row
+    p = Photo.find(params[:toast_id])
+
+    p.destroy
+
+    @remaining_count = Photo.count
+
+    render("pic_templates/destroy_row.html.erb")
+  end
 end
